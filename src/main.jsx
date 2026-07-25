3eef78ff23950837423da5d1d2f43fee3600f071
scarryhott <118125945+scarryhott@users.noreply.github.com>
2026-07-25 11:34:35 -0400

    Remove unused application providers

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import NfcStdbRootProvider from './spacetimedb/NfcStdbRootProvider.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-        <NfcStdbRootProvider>
-            <App />
-        </NfcStdbRootProvider>
-    </React.StrictMode>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 )
