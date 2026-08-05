da6b03548555df077e7f450eeb9a99ecd9169b2a
scarryhott <118125945+scarryhott@users.noreply.github.com>
2026-08-05 14:18:34 -0400

    Render visible React deployment marker

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import DeploymentMarker from './DeploymentMarker.jsx'
 import './styles.css'
 import './closure-tokenomics.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <DeploymentMarker />
   </React.StrictMode>,
 )
