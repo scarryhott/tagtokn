b2ac4bd5047a18eecdf58d9aca88d0f7b55ed571
scarryhott <118125945+scarryhott@users.noreply.github.com>
2026-08-05 17:04:48 -0400

    Use the preserved unified application entry point

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import DeploymentMarker from './DeploymentMarker.jsx'
-import './app.css'
-import './styles.css'
-import './closure-tokenomics.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    <DeploymentMarker />
   </React.StrictMode>,
 )
