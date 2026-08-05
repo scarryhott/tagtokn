bcf9c68a769716fd9e387429cda631034b8a489d
scarryhott <118125945+scarryhott@users.noreply.github.com>
2026-08-05 15:54:33 -0400

    Fix Vercel stylesheet resolution

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import DeploymentMarker from './DeploymentMarker.jsx'
+import './app.css'
 import './styles.css'
 import './closure-tokenomics.css'
 
